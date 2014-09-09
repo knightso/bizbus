dfc2f0b6e0ee8de10f16fab33168beb6590d7dcc
xutaowang <sino.wxt@knightso.co.jp>
2014-09-09 09:43:17 +0800

    add tg.stops feature finished.

--- src-static/mng/app/scripts/controllers/tripgroupstops.js
+++ src-static/mng/app/scripts/controllers/tripgroupstops.js
@@ -8,16 +8,29 @@
  * Controller of the mngApp
  */
 angular.module('mngApp')
-  .controller('TripgroupstopsCtrl', function ($scope, $routeParams, $http, Tripgroups) {//Stations
+  .controller('TripgroupstopsCtrl', function ($scope, $routeParams, $timeout, Tripgroups, Stations) {
     $scope.charLimit = 15;
     $scope.agencyId = $routeParams.agencyId;
     $scope.routeId = $routeParams.routeId;
     $scope.tripgroupId = $routeParams.tripgroupId;
+    $scope.detailMode = true;
 
+    $scope.selectedStops = [];
     $scope.tg = Tripgroups.get({
       agencyId : $routeParams.agencyId,
       routeId : $routeParams.routeId,
       id : $routeParams.tripgroupId
+    });
+    $scope.tg.$promise.then(function(tg) {
+      if (tg !== undefined) {
+        $scope.tg.agencyId = $routeParams.agencyId;
+        angular.forEach(tg.stops, function(stopId) {
+          var stop = Stations.get({
+            id : stopId
+          });
+          $scope.selectedStops.push(stop);
+        });
+      }
     });
 
     $scope.stops = [];
@@ -25,24 +38,67 @@ angular.module('mngApp')
       if (query === undefined || query === '') {
         return;
       }
-      var params = {query: query};
-      return $http.get(
-        '/api/stops',
-        {params: params}
-      ).then(function(response) {console.log(response.data.length);
-        $scope.stops = response.data;
+      $scope.stops = Stations.queryStops({
+        query : query
       });
     };
 
-    $scope.selectedStops = [];$scope.index=1;
-    $scope.addStop = function(stop) {console.log(stop);
-      //$scope.selectedStops.push(stop);
-      $scope.selectedStops.push({id:$scope.index, name:'bb'+$scope.index});
-$scope.index=$scope.index+1;
+    $scope.selectedStop = {};
+    $scope.addStop = function(stop) {
+      var needAdd = true;
+      angular.forEach($scope.selectedStops, function(selectedStop) {
+        if (selectedStop.id === stop.id) {
+          needAdd = false;
+        }
+      });
+      if (needAdd) {
+        $scope.selectedStops.push(stop);
+      }
     };
     
     $scope.removeStop = function(stop) {
       var index = $scope.selectedStops.indexOf(stop);
-      $scope.selectedStops.splice(index, 1);
+      if (index !== -1) {
+        $scope.selectedStops.splice(index, 1);
+      }
+    };
+
+    $scope.submit = function() {
+
+      $scope.alerts = [];
+      $scope.error = {};
+
+      var saveStops = [];
+      for (var i = 0; i < $scope.selectedStops.length; i++) {
+        saveStops.push($scope.selectedStops[i].id);
+      }
+      $scope.tg.stops = saveStops;
+      console.log($scope.tg.stops);
+
+      var start = +new Date();
+      var tout = 100;
+      var doSave = function(actualF) {
+        actualF($scope.tg,
+          function() {
+            var tat = +new Date() - start;
+              $timeout(function() {
+                $scope.alerts.push({type: 'success', msg: '保存に成功しました。'});
+                $scope.detailMode = true;
+              }, (tat >= tout ? 0 : tout - tat));
+            },
+            function() {
+              var tat = +new Date() - start;
+              $timeout(function() {
+                $scope.alerts.push({type: 'danger', msg: '保存に失敗しました。'});
+              }, (tat >= tout ? 0 : tout - tat));
+            }
+        ); 
+      };
+
+      doSave(Tripgroups.update);
+    };
+
+    $scope.closeAlert = function(index) {
+      $scope.alerts.splice(index, 1);
     };
   });
